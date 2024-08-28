f63a38ee773d461ea39bca3a096fd438eac8125a
itsejmendoza <elthonjohnmendoza4@gmail.com>
2024-08-28 12:41:25 +0800

    string-handling.py

---
+++ Exercise 02/string-handling.py
@@ -0,0 +1,9 @@
+def color_name():
+    
+    color1 = input("Enter the first color: ")
+    color2 = input("Enter the second color: ")
+    color3 = input("Enter the third color: ")
+
+    print(f"{color1}, {color2}, and {color3} created a new color!")
+
+color_name()
